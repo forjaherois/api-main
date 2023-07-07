a34b7db70802054409fa42b4cfac40efc2bb73c4
Daniel Barreto <munizbarreto5@gmail.com>
2023-07-07 08:08:54 -0300

    tetste e commit

--- src/enviorement-test.test.ts
+++ src/enviorement-test.test.ts
@@ -2,5 +2,5 @@ import Circle from './enviorement-test';
 
 test('Should calculate the area of circle', () => {
     const circle = new Circle(2);
-    expect(circle.getArea()).toBe(12.566370614359172);
+    expect(circle.getArea()).toBe(1.566370614359172);
 });
