289d11fc45979efcd8d28dfba8f4e0429933e792
Daniel Barreto <munizbarreto5@gmail.com>
2023-07-07 08:01:36 -0300

    chore: start project

---
+++ src/enviorement-test.ts
@@ -0,0 +1,13 @@
+export default class Circle {
+    radius: number;
+
+    constructor(radius: number) {
+        this.radius = radius;
+        const teste =
+            'AIahiUHAIUhiauhIUHAIhaiUHAIUhaiuhIUHiuhIUSHIaushiuAHSIUahsiuhASIUHaisuhAIUSHAiuhsiuAHSIUxkjzlxkjzlxjsaSJoiajsoIAJSOIjakxji';
+    }
+
+    getArea(): number {
+        return 2 * Math.PI * this.radius;
+    }
+}
