dc2c79c42bc9c0090660b8e65801d340e97a8d58
Daniel Barreto <munizbarreto5@gmail.com>
2023-07-08 13:11:37 -0300

    refactor: adjustment initial server process

--- src/configs/http-server.ts
+++ src/configs/http-server.ts
@@ -1,11 +1,30 @@
-import fastify from "fastify";
+import { FastifyInstance } from "fastify";
 
 import { RouterFactory } from "./router/route-factory";
 import { RouteRegister } from "./router/route-register";
 
-export const httpServer = fastify();
+export class HttpServer {
+    constructor(
+        private host: string,
+        private port: number,
+        private server: FastifyInstance
+    ) {}
 
-const routes = RouterFactory.createRoutes();
-const routeRegister = new RouteRegister(httpServer, routes);
+    private registerRoutes() {
+        const routes = RouterFactory.createRoutes();
+        const routeRegister = new RouteRegister(this.server, routes);
+        routeRegister.registerAllRoutes();
+    }
 
-routeRegister.registerAllRoutes();
+    async start() {
+        try {
+            this.registerRoutes();
+
+            await this.server.listen({ port: this.port, host: this.host });
+            console.log(`✔ ONLINE SERVER AT: http://localhost:${this.port}`);
+        } catch (err) {
+            console.error("Error starting the server:", err);
+            process.exit(1);
+        }
+    }
+}
