75d408c360db08bd1986d6192ecbd489446f645f
Daniel Barreto <munizbarreto5@gmail.com>
2023-07-13 22:49:03 -0300

    feat: add readme file

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,13 @@
-import type { Config } from "@jest/types";
+import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
     clearMocks: true,
     bail: true,
-    coverageProvider: "v8",
-    preset: "ts-jest",
-    testMatch: ["**/*.test.ts"],
+    coverageProvider: 'v8',
+    preset: 'ts-jest',
+    testMatch: ['**/*.test.ts'],
     moduleNameMapper: {
-        "^@src/(.*)$": "<rootDir>/src/$1",
+        '^@src/(.*)$': '<rootDir>/src/$1',
     },
 };
 
